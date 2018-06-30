93bc81cc974b90e3b4ba171bc1451886b65e1b0a
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-07-01 01:57:21 +0530

    Understanding Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve('stable');
+    },2000);
+  });
+
+  servers=[
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+
+  filteredStatus= '';
+
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}){
+    return{
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
+
+  onAddsServer(){
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15,1,2017)
+    });
+  }
+
 }
